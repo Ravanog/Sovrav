9aae26525570dac2a00ec64c8de017091bde5511
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2024-02-01 17:21:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -110,12 +110,6 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('♦ 𝐌𝐨𝐯𝐢𝐞 𝐆𝐫𝐩 ♦', url=f"https://telegram.me/+yfnfEehsOyhiZmI9"), 
-                    InlineKeyboardButton('🔰 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🔰', url=f"https://telegram.me/+qcIFCI0NT2g5ODhl") 
-                ],[
-                    InlineKeyboardButton('❄ 𝐉𝐎𝐈𝐍 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ❄', url=f"https://telegram.me/TG_LINKS_CHANNEL2")
-                ],[
-                    InlineKeyboardButton("😈 𝐀𝐛𝐨𝐮𝐭 𝐌𝐞 😈", callback_data = "about"),
                     InlineKeyboardButton("❗❗ 𝐂𝐥𝐨𝐬𝐞 ❗❗", callback_data = "close")
                 ]
             ]
