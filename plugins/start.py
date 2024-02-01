af52f8297327cb2db3700e471ae30eba8598f39a
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2024-02-01 17:27:31 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -80,7 +80,7 @@ async def start_command(client: Client, message: Message):
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup,
                                           protect_content=PROTECT_CONTENT)
                 await snt_msg.reply_text(
-                    f"💢 Files will be deleted in 3 minutes to avoid copyright issues ‼️. Please forward and save them ♻️.",
+                    f"💢 Files will be deleted in 30 minutes to avoid copyright issues ‼️. Please forward and save them ♻️.",
                     disable_web_page_preview=True,
                     quote=True
                 )
@@ -91,7 +91,7 @@ async def start_command(client: Client, message: Message):
                                           protect_content=PROTECT_CONTENT)
                 snt_msgs.append(snt_msg)
                 await snt_msg.reply_text(
-                    f"💢 Files will be deleted in 3 minutes to avoid copyright issues ‼️. Please forward and save them ♻️.",
+                    f"💢 Files will be deleted in 30 minutes to avoid copyright issues ‼️. Please forward and save them ♻️.",
                     disable_web_page_preview=True,
                     quote=True
                 )
